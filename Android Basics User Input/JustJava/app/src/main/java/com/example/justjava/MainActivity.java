0bca72b2de0f8315967562a84f32cbcd66bbc46d
Ahmed Sayed Mansour <ahmed.mans20719@gmail.com>
2020-07-06 08:00:24 +0200

    arabic and french languages and features added

--- Android Basics User Input/JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ Android Basics User Input/JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -13,6 +20,8 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
 
     int quantity = 0;
+    boolean whippedCream =false, chocolate = false;
+    Editable name ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +32,25 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "total: $"+ quantity *5 + "\nThank you!";
-        displayMessage(priceMessage);
+        EditText nameEditText = (EditText) findViewById(R.id.name_EditText);
+        name = nameEditText.getText();
+        CheckBox whippedCreamView = (CheckBox) findViewById(R.id.whipped_cream_view);
+        if(whippedCreamView.isChecked()){
+            whippedCream = true;
+        }else{
+            whippedCream = false;
+        }
+        CheckBox ChocolateView = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        if(ChocolateView.isChecked()){
+            chocolate = true;
+        }else{
+            chocolate = false;
+        }
+        String priceMessage =   "Name : "+ name +"\n"+getString(R.string.addWhipped)+whippedCream +"\n"+getString(R.string.addChoco)+chocolate+
+                                "\nQuantity: "+quantity+"\ntotal: $"+ (quantity *5+ (whippedCream?1:0) *quantity + (chocolate?2:0) *quantity)
+                                + "\nThank you!";
+        composeEmail(priceMessage,"Just Java order for " + name);
+//        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
@@ -48,16 +74,26 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+    /*private void displayPrice(int number) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }*/
 
     /**
      * This method displays the given text on the screen.
      */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+    /*private void displayMessage(String message) {
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+    }*/
+
+    public void composeEmail(String body, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_TEXT, body);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
